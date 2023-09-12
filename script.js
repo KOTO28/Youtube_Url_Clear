8ab1510869f32df843ef047da1c9dfd0e07fa2d9
KOTO28 <k.tonton128@gmail.com>
2023-09-13 08:39:57 +0900

    update script.js

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function clear() {
-	data_0 = location.href//<https://www.youtube.com/watch?v=Mmw1oRpkMHE&lc=UgzKtaL1wXStXSDNyZZ4AaABAg>
-	data_1 = data_0.split('/');
-	data_2 = data_1[3].split('&');
-	url = data_1[0]+"//"+data_1[2]+"/"+data_2[0]
-	//alert(data_0+"\n"+url);
+	var data_0 = location.href;
+	var data_1 = data_0.split('watch?v=');
+	var data_2 = null;
+	var url = null;
+	if (data_1[1] != undefined){
+		// console.log("This is video.")
+		data_1 = data_0.split('/');
+		data_2 = data_1[3].split('&');
+		url = data_1[0]+"//"+data_1[2]+"/"+data_2[0];
+	}else{
+		// console.log("This is community.")
+		data_1 = data_0.split('/community?lc=');
+		if (data_1[1] != undefined){
+			data_2 = data_1[1].split('lb=');
+			url = data_1[0]+"/community?lb="+data_2[1];
+		}else{
+			// console.log("This is else.")
+			url = location.href;
+		}
+	}
+	//console.log("url:"+url);
 	location.href = url;
 }
